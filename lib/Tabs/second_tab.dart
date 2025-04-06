4cfd78130ebac9a2594cf712ceae644c4778a351
tamasdarocz <tamasdarocz@protonmail.com>
2025-04-07 00:13:00 +0200

    Update second_tab.dart AppBar

--- lib/Tabs/second_tab.dart
+++ lib/Tabs/second_tab.dart
@@ -5,6 +5,9 @@ class SecondTab extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Center(
+appBar: AppBar (
+centerTitle: true,
+title: Text('Authenticator'), backgroundColor: Colors.amber),
       child: Text('2'),
     );
   }
