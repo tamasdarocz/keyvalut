4bc35a2a16136f32aad12b5100aaaacac8c3d1ba
tamasdarocz <tamasdarocz@protonmail.com>
2025-04-08 23:02:23 +0200

    reshape

--- lib/Tabs/first_tab.dart
+++ lib/Tabs/first_tab.dart
@@ -15,90 +15,98 @@ class _FirstTabState extends State<FirstTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Passwords'),
-        backgroundColor: Colors.amber,
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: [
-          TextField(
-            scrollPadding: EdgeInsets.all(50),
-            style: TextStyle(fontSize: 20),
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.devices),
-              label: Text('Platform:'),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(48),
-                borderSide: BorderSide(color: Colors.amber, width: 4),
-              ),
-            ),
-          ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Passwords'),
+          backgroundColor: Colors.amber,
+        ),
+        body: ListView(
+            padding: EdgeInsets.all(16),
+            children: [
 
-          TextField(
-            style: TextStyle(fontSize: 20),
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.email),
-              label: Text('Email:'),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(48),
-                borderSide: BorderSide(color: Colors.amber, width: 4),
-              ),
-            ),
-          ),
+              Divider(height: 10, color: Colors.white),
 
-          TextField(
-            style: TextStyle(fontSize: 20),
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.person),
-              label: Text('Username'),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(48),
-                borderSide: BorderSide(color: Colors.amber, width: 4),
-              ),
-            ),
-          ),
-
-          TextField(
-            style: TextStyle(fontSize: 20),
-            obscureText: !this._showPassword,
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.key),
-              suffixIcon: IconButton(
-                icon: Icon(
-                  Icons.remove_red_eye,
-                  color: this._showPassword ? Colors.amber : Colors.grey,
+              TextField(
+                scrollPadding: EdgeInsets.all(50),
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.devices),
+                  label: Text('Platform:'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(48),
+                    borderSide: BorderSide(color: Colors.amber, width: 4),
+                  ),
                 ),
-                onPressed: () {
-                  setState(() => this._showPassword = !this._showPassword);
-                },
               ),
-              label: Text('Password'),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(48),
-                borderSide: BorderSide(color: Colors.amber, width: 4),
+
+              Divider(height: 10, color: Colors.white),
+
+              TextField(
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  label: Text('Email:'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(48),
+                    borderSide: BorderSide(color: Colors.amber, width: 4),
+                  ),
+                ),
               ),
-            ),
-          ),
 
-          IconButton(onPressed: () {} , icon: Icon(Icons.add)),
+              Divider(height: 10, color: Colors.white),
 
-
-
-          TextField(
-            style: TextStyle(fontSize: 20),
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.alternate_email),
-              label: Text('Website'),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(48),
-                borderSide: BorderSide(color: Colors.amber, width: 4),
+              TextField(
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  label: Text('Username'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(48),
+                    borderSide: BorderSide(color: Colors.amber, width: 4),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ],
-      ),
-    );
+
+              Divider(height: 10, color: Colors.white),
+
+              TextField(
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.alternate_email),
+                  label: Text('Website'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(48),
+                    borderSide: BorderSide(color: Colors.amber, width: 4),
+                  ),
+                ),
+              ),
+
+              Divider(height: 10, color: Colors.white),
+
+              TextField(
+                  style: TextStyle(fontSize: 20),
+                  obscureText: !this._showPassword,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.key),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        Icons.remove_red_eye,
+                        color: this._showPassword ? Colors.amber : Colors.grey,
+                      ),
+                      onPressed: () {
+                        setState(() =>
+                        this._showPassword = !this._showPassword);
+                      },
+                    ),
+                    label: Text('Password'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(48),
+                      borderSide: BorderSide(color: Colors.amber, width: 4),
+                    ),
+                  ))
+            ]));
   }
 }
+
+
+
